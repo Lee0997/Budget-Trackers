1d1312aa80dc00b54239779b361b49aa2d2aef19
Lee <leenock2012@yahoo.co.uk>
2021-12-18 19:10:27 +0000

    Added manifest and linked in index

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,27 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/styles.css"];
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/manifest.webmanifest",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
 
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
-      .open(PRECACHE)
+      .open(STATIC_CACHE)
       .then((cache) => cache.addAll(FILES_TO_CACHE))
       .then(self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
   event.waitUntil(
     caches
       .keys()
@@ -41,7 +49,7 @@ self.addEventListener("fetch", (event) => {
           return cachedResponse;
         }
 
-        return caches.open(RUNTIME).then((cache) => {
+        return caches.open(RUNTIME_CACHE).then((cache) => {
           return fetch(event.request).then((response) => {
             return cache.put(event.request, response.clone()).then(() => {
               return response;
